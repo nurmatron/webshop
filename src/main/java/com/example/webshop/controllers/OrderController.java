e8b4c6ed36f833e20ee1b2bf517cef047b8a4069
Golaj <ch.rehn90@gmail.com>
2019-12-02 13:31:37 +0100

    Crud with service for Order created.

--- src/main/java/com/example/webshop/controllers/OrderController.java
+++ src/main/java/com/example/webshop/controllers/OrderController.java
@@ -1,4 +1,50 @@
 package com.example.webshop.controllers;
 
+import com.example.webshop.models.Order;
+import com.example.webshop.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/order")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    //create
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.create(order);
+    }
+
+    //readOne
+    @GetMapping
+    @RequestMapping(path = "get/{id}")
+    public Order getOneOrder(@PathVariable Integer id) {
+        return orderService.getOne(id);
+    }
+
+    //readAll
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAll();
+    }
+
+    //Udpate
+    @PutMapping
+    @RequestMapping(path = "update/{id}")
+    public Order updateOrder(@PathVariable Integer id, @RequestBody Order order) {
+        return orderService.update(id, order);
+    }
+
+    //delete
+    @DeleteMapping
+    @RequestMapping(path = "delete/{id}")
+    public void deleteOrder(@PathVariable Integer id){
+        orderService.delete(id);
+    }
+
 }
---
+++ src/main/java/com/example/webshop/services/OrderService.java
@@ -0,0 +1,43 @@
+package com.example.webshop.services;
+
+import com.example.webshop.models.Order;
+import com.example.webshop.repositories.OrderRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService implements CrudService<Order>{
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public Order create(Order order) {
+        return orderRepository.saveAndFlush(order);
+    }
+
+    @Override
+    public Order getOne(Integer id) {
+        return orderRepository.getOne(id);
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order update(Integer id, Order order) {
+        Order existingOrder = getOne(id);
+        BeanUtils.copyProperties(order, existingOrder, "id");
+        return orderRepository.saveAndFlush(existingOrder);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        orderRepository.deleteById(id);
+    }
+}
